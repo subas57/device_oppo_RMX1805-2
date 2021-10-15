48b823db431bf973cba856f05666ab3b1c031433
jaideepgithub <jaideepcool999@gmail.com>
2021-10-15 12:39:08 +0530

    RMX1805: pixelify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_RMX1805.mk
+    $(LOCAL_DIR)/aosp_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_RMX1805-user \
-    octavi_RMX1805-userdebug \
-    octavi_RMX1805-eng
+    aosp_RMX1805-user \
+    aosp_RMX1805-userdebug \
+    aosp_RMX1805-eng
--- aosp_RMX1805.mk
+++ aosp_RMX1805.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common octavi stuff
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common aosp stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,12 +14,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := octavi_RMX1805
+PRODUCT_NAME := aosp_RMX1805
 PRODUCT_MODEL := Realme 2/C1
 
-OCTAVI_DEVICE_MAINTAINER := JAIDEEP
-
-# PRODUCT_GMS_CLIENTID_BASE := android-oppo
+WITH_GAPPS := true
+IS_PHONE := true
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 TARGET_VENDOR := oppo
 TARGET_VENDOR_PRODUCT_NAME := RMX1805
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -28,5 +28,5 @@ TARGET_USES_BLUR := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1805"
 
-#TARGET_GAPPS_ARCH := ARM64
+TARGET_GAPPS_ARCH := ARM64
 TARGET_BOOT_ANIMATION_RES := 720
