b4f80385bf9dee50b9e6f014e5f3c33902006850
Subas <77564932+subas57@users.noreply.github.com>
2021-10-18 12:34:15 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_RMX1805.mk
+    $(LOCAL_DIR)/corvus_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_RMX1805-user \
-    octavi_RMX1805-userdebug \
-    octavi_RMX1805-eng
+    corvus_RMX1805-user \
+    corvus_RMX1805-userdebug \
+    corvus_RMX1805-eng
